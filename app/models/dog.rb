afd60714da8d9dc196e94d40874377b0433deac3
John Carter <johncarter352@gmail.com>
2020-11-14 23:44:37 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,16 @@
 class Dog
   attr_accessible :name, :breed, :age
   
+  all = []
+  
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    self << all
   end
   
+  def self.all 
+    
   
 end
